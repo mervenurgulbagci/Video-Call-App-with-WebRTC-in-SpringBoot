202f62446617d778e134e8aa29c8d0d0774e19d1
Merve Nurgül Bağcı <mervenurgulbagci@gmail.com>
2020-12-20 16:44:48 +0300

    Initial commit

---
+++ src/main/java/com/mnb/WebSocketConfiguration.java
@@ -0,0 +1,15 @@
+package com.mnb;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfiguration implements WebSocketConfigurer{
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new SocketHandler(),"/socket").setAllowedOrigins("*");
+    }
+}
---
+++ src/test/java/com/mnb/VideoCallAppWithWebRtcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoCallAppWithWebRtcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
